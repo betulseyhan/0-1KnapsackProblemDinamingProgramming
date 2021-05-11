91a3f0176d1553c0a39dfac0fca3fe7d3da92347
Betül Seyhan <78261821+betulseyhan@users.noreply.github.com>
2021-05-11 20:58:29 +0300

    Add files via upload

--- RecursiveSolution.java
+++ RecursiveSolution.java
@@ -5,19 +5,15 @@ import java.util.HashMap;
 
 public class RecursiveSolution {
     private KnapsackK knapsack;
-    private int[][] decision;
-    private int[][] objFunc;
     private ArrayList<ItemK> item;
-    //private HashMap<String, Boolean> decisions;
-    //private HashMap<String, Integer> objFunc;
+    private HashMap<String,Integer> objFunc;
+    private HashMap<String,Boolean> decision;
+
 
     public RecursiveSolution(KnapsackK knapsack) {
         this.knapsack = knapsack;
-        int x = this.knapsack.knapsackCapacity + 1;
-        //this.decisions = new HashMap<String, Boolean>();
-        //this.objFunc=new HashMap<String, Integer>();
-        this.objFunc = new int[x][x];
-        this.decision = new int[x][x];
+        this.objFunc = new HashMap<String,Integer>();
+        this.decision =new HashMap<String,Boolean>();
         this.item = new ArrayList<ItemK>();
         for (int i = 0; i < knapsack.items.length; i++) {
             this.item.add(this.knapsack.items[i]);
@@ -26,41 +22,47 @@ public class RecursiveSolution {
     }
 
     public int recursion(int index, int remainingCapacity) {
-        int take = 0;
-        int leave = 0;
+
         int w = this.item.get(index).getWeight();
         int v = this.item.get(index).getValue();
-        if (index == 0) {
-            if (w <= remainingCapacity) {
-                this.objFunc[index][remainingCapacity] = 1;
-                this.decision[index][remainingCapacity] =this.item.get(0).getValue();
-                return this.item.get(0).getValue();
-            } else {
-                // current item not picked as its size exceeds the capacity of the sack
-                // hence value not stored
-                this.objFunc[index][remainingCapacity] = 0;
-                this.decision[index][remainingCapacity] = 0;
+        boolean check=index!=0;
+
+        if(check){
+            int include=0;
+            int exclude=0;
+
+            if (remainingCapacity>=w) {
+                include = recursion(index - 1, remainingCapacity - w) + v;
+                exclude = recursion(index - 1, remainingCapacity);
+                if (include < exclude) {
+                    this.decision.put((index+"/"+remainingCapacity) , false);
+                }
+                else if(include > exclude){
+                    this.decision.put((index+"/"+remainingCapacity) , true);
+                }
+                else{
+                    this.decision.put((index+"/"+remainingCapacity) , true);
+                }
+            }
+            this.objFunc.put((index+"/"+remainingCapacity) ,Math.max(include, exclude));
+        }
+
+        else {
+            if (!(w <= remainingCapacity)) {
+
+                this.decision.put((index + "/" + remainingCapacity), false);
+                this.objFunc.put((index + "/" + remainingCapacity), 0);
                 return 0;
             }
+            if ( w <= remainingCapacity) {
+                this.decision.put((index + "/" + remainingCapacity), true);
+                this.objFunc.put((index + "/" + remainingCapacity), this.item.get(0).getValue());
+                return this.item.get(0).getValue();
+            }
+
         }
-        if (w <= remainingCapacity)
-            take = recursion(index - 1, remainingCapacity - w) + v;
 
-        // Choice 2 - of not picking it up.
-        leave = recursion(index - 1, remainingCapacity);
-
-        // optimal solution, Maximized value stored
-        decision[index][remainingCapacity] = Math.max(take, leave);
-
-        // if the item which i am picking gives the optimal solution then
-        // i will store it
-        if (take > leave) {
-            objFunc[index][remainingCapacity] = 1;
-        } else {
-            objFunc[index][remainingCapacity] = 0;
-        }
-        // returning the optimal solution
-        return decision[index][remainingCapacity];
+        return this.objFunc.get((index+"/"+remainingCapacity));
 
     }
 
